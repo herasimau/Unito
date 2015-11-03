99a59160665c2f61e0deb6f6a2112f55bd034ec6
Leanid <leanid.herasimau@edu.unito.it>
2015-11-03 23:11:18 +0100

    a4

--- esercizio.a4/src/Main.java
+++ esercizio.a4/src/Main.java
@@ -5,15 +5,15 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Insert string:");
-        String string = in.nextLine();
-       for (int k=0; k< string.length();k++ )
+        String inputUser = in.nextLine();
+       for (int k=0; k< inputUser.length();k++ )
        {
-           char c = string.charAt(k);
+           char c = inputUser.charAt(k);
 
-           System.out.print((int) c + " ");
+           System.out.print((int)c + " ");
         }
         System.out.print("\n");
-        System.out.print("Numero caratteri: " + string.length());
+        System.out.print("Numero caratteri: " + inputUser.length());
         }
     }
 
