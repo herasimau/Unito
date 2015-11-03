20f9ecd4af3b37da7bf78f6286b55a928604e5a9
Leanid <leanid.herasimau@edu.unito.it>
2015-11-03 20:55:26 +0100

    a6

--- esercizio.a6/src/Main.java
+++ esercizio.a6/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -8,43 +7,34 @@ public class Main {
 
         String si = "s";
         String no = "n";
-        ArrayList answers = new ArrayList(); //Salva risposta qui
+        Scanner in = new Scanner(System.in);
+        ArrayList answers = new ArrayList(); //Salvare risposta qui
         String questions [] = {"Hai svolto tutti i tuoi compiti a casa?","E' venerdi?","Ce' qualcosa interessante alla TV",
-                "Sta piovendo?" }; //Tutti domande
-        for (int i=0;i<questions.length;i++){ //Per non ripetre ogni volta ho fatto for loop
-            Scanner in = new Scanner(System.in);
+                "Sta piovendo?" }; //Tutte le domande
+        for (int i=0;i<questions.length;i++) { //Loop per le domande
             System.out.println(questions[i]);
-            String k = in.nextLine();
-            if(!k.equals(si)&&!k.equals(no)) //Controllo se utente inserisce un carattere diverso da s or n, stampa errore e esci dal lopp
-            {
-                System.out.println("you can answer only s or n");break;
-            }
-            else if (!k.equals(si)){ //Se utente ha risposto s, aggiungo in Arraylist di risposte
 
-                answers.add("s");
-
-            }
-            else if (!k.equals(no)){ //Se utente ha risposto n, aggiungo in Arraylist di risposte
-
-                answers.add("n");
-
+            boolean inputIsOk = false;
+            while (!inputIsOk) {
+                try {
+                    String answer = in.nextLine();
+                    if (answer.equals(si) || answer.equals(no))
+                    {
+                        inputIsOk = true;
+                        answers.add(answer);
+                    }
+                    else { System.out.println("Puoi rispondere solo s o n");
+                    }
+                } catch (NumberFormatException error)
+                        {error.getMessage();}
             }
         }
 
-        int a = answers.size(); //La grandezza delle risposte
-
-       if(a==4) //Se la grandezza e' uguale a 4 andiamo avanti, altrimenti terminiamo il programma
-        {
-            Collections.reverse(answers);// Mettiamo le risposte in ordine
-            if (answers.get(0) == si && answers.get(1) == si && answers.get(2) == no && answers.get(3) == no) {
-                System.out.println("Puoi uscire"); //Controllo se risposte corrispondono a schema s,s,n,n
+            if (answers.get(0).equals(si) && answers.get(1).equals(si) && answers.get(2).equals(no) && answers.get(3).equals(no)) {
+                System.out.println("Puoi uscire"); //Controllo se le risposte corrispondono a schema s,s,n,n
             } else {
                 System.out.println("Non puoi uscire");
             }
-
-        }
-        else {System.exit(0);}
-
 
     }
 }
