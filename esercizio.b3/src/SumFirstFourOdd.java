2e013c6e07893a3c94a16b672695158d91875edb
Leanid <leanid.herasimau@edu.unito.it>
2015-11-10 23:49:45 +0100

    b3

--- esercizio.b3/src/SumFirstFourOdd.java
+++ esercizio.b3/src/SumFirstFourOdd.java
@@ -1,27 +1,31 @@
 
 public class SumFirstFourOdd {
-    public static boolean isEven (int num) {
+    public static boolean isEven(int num) {
         return (num & 1) == 0;
     }
     public static void main(String[] args) {
-	    int count =0;
-        int sum =0;
-     do {
+        int count = 0;
+        int sum = 0;
+        int input;
+        
+        do {
             Display.println("Insert odd number: ");
-            int input = Keyboard.readInteger();
-            for(;!isEven(input);)
-            {
+            input = Keyboard.readInteger();
+
+            for (; !isEven(input); ) {
                 sum += input;
                 count++;
                 break;
             }
-            for(;isEven(input);)
-            {
+            for (; isEven(input); ) {
                 Display.println("Error, you need to insert odd number");
                 break;
             }
 
-       } while (count<4);
-        Display.print("The sum of first 4 odd numbers is: "+sum);
+        } while (count < 4 &&input!=-1);
+
+        switch (count){
+       case 4: Display.print("The sum of first 4 odd numbers is: " + sum);}
     }
+
 }
