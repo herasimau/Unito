4a4976387f9b605b7aac06f710c40c38c5d8539c
Leanid <leanid.herasimau@edu.unito.it>
2015-11-11 00:07:12 +0100

    sum odd using IF ELSE

---
+++ esercizio.b3/src/SumOdd.java
@@ -0,0 +1,28 @@
+
+public class SumOdd {
+
+    public static void main(String[] args) {
+        int count =0;
+        int sum =0;
+        for(;count<4;)
+        {
+            Display.println("Insert odd number: ");
+            int input = Keyboard.readInteger();
+            if (input%2!=0)
+            {
+                sum +=input;
+                count++;
+            }
+            else if (input==-1)
+            {
+                Display.println("You choose to exit");
+                break;
+            }
+            else if(input%2==0)
+            {
+                Display.println("Error. You need to insert odd number");
+            }
+        }
+        if(count==4) { Display.println("The sum of 4 odd numbers is "+sum); }
+    }
+}
