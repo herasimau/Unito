f6a79b26d74d93e07f43e977f256abe773b99498
Leanid <leanid.herasimau@edu.unito.it>
2015-11-10 21:54:53 +0100

    sum of first 4 odd numbers

---
+++ esercizio.b3/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+    public static boolean isEven (int num) {
+        return (num & 1) == 0;
+    }
+    public static void main(String[] args) {
+	    int count =0;
+        int sum =0;
+     do {
+            Display.println("Insert odd number: ");
+            int input = Keyboard.readInteger();
+            for(;!isEven(input);)
+            {
+                sum += input;
+                count++;
+                break;
+            }
+            for(;isEven(input);)
+            {
+                Display.println("Error, you need to insert odd number");
+                break;
+            }
+
+       } while (count<4);
+        Display.print("The sum of first 4 odd numbers is: "+sum);
+    }
+}
