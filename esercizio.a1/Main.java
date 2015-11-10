8e9dfd739ddb548270016cf472287e7cb5a0e8e0
leanid herasimau <herasimau.leanid@gmail.com>
2015-11-10 16:39:50 +0100

    with I/O classes.

--- esercizio.a1/Main.java
+++ esercizio.a1/Main.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int i;
-        System.out.print("Inserisci un numero intero: ");
-        if(sc.hasNextInt()) {
-            i = sc.nextInt();
-            System.out.println("Hai inserito il numero: "+i);
-        } else {
-            System.out.println("Errore");
-        }
+
+        Display.println("Insert int: ");
+        Display.print("You insert: "+Keyboard.readInteger());
     }
 }
