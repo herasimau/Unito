8acee3f9888b3dba01b110d6a4df99ca3e84cb75
leanid herasimau <herasimau.leanid@gmail.com>
2015-11-10 18:20:10 +0100

    with I/O classes.

--- esercizio.a3bis/src/Main.java
+++ esercizio.a3bis/src/Main.java
@@ -1,33 +1,27 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        while (true) {
-            System.out.println("a. New document");
-            System.out.println("b. Open document");
-            System.out.println("c. Save as ...");
-            System.out.println("d. Mail document to...");
-            System.out.println("e. Exit");
-
-            Scanner in = new Scanner(System.in);
-            System.out.println("Please make your choice (a-d): ");
-            String inputUser = in.nextLine();
-
-            if (inputUser.equals("a") || inputUser.equals("A")) {
-                System.out.println("You chose to create new document");
-            } else if (inputUser.equals("b") || inputUser.equals("B")) {
-                System.out.println("You chose to open document");
-            } else if (inputUser.equals("c") || inputUser.equals("C")) {
-                System.out.println("You chose to save current document");
-            } else if (inputUser.equals("d") || inputUser.equals("D")) {
-                System.out.println("You chose to send via email current document");
-            } else if (inputUser.equals("e") || inputUser.equals("E")) {
+        int inputAnswer;
+        do
+         {
+            Menu.printMenu();
+            inputAnswer = Keyboard.readCharacter();
+            if (inputAnswer == 'a') {
+                Display.println("You choose to create a new document");
+            } else if (inputAnswer == 'b') {
+                Display.println("You choose to open document");
+            } else if (inputAnswer == 'c') {
+                Display.println("You choose to save document");
+            } else if (inputAnswer == 'd') {
+                Display.println("You choose to send document");
+            } else if (inputAnswer != 'e') {
+                Display.println("Your choice is not valid");
                 break;
-            } else {
-                System.out.println("Errore");
             }
-        }
+             do
+                 inputAnswer = Keyboard.readCharacter();
+             while (inputAnswer !='\n' );
+        } while (inputAnswer !='e' );
     }
 }
---
+++ esercizio.a3bis/src/Menu.java
@@ -0,0 +1,14 @@
+
+public class Menu {
+    private Menu(){}
+    public static void printMenu()
+    {
+        Display.println("a. New document");
+        Display.println("b. Open document");
+        Display.println("c. Save as ...");
+        Display.println("d. Mail document to...");
+        Display.println("e. Exit");
+        Display.println("Please make your choice (a-e): ");
+    }
+
+}
