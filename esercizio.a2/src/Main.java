7beb98a4d38cb28534bb9f2675d7e47abaf6b496
leanid herasimau <herasimau.leanid@gmail.com>
2015-11-10 16:47:07 +0100

    with I/O classes.

--- esercizio.a2/src/Main.java
+++ esercizio.a2/src/Main.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
 
 public class Main
 {
     public static void main (String[] args)
     {
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Insserisci un solo carratere: ");
-        String inputUser = in.nextLine();
-
-
-        if (inputUser.length() > 1 || inputUser.isEmpty() )
-        {
-            System.out.println("Errore");
-        }
-        else
-        {
-            System.out.println("Hai inserito "+inputUser);
-        }
-
+        int input;
+        Display.println("Insert one char: ");
+        input = Keyboard.readCharacter();
+        Display.println("You have insert: " +(char)input);
+        Display.print("Int value for "+(char)input+" is "+input);
     }
 }
