a21b48180d12891895ae45867a4764b13c3d3156
Leanid <leanid.herasimau@edu.unito.it>
2015-11-11 18:37:08 +0100

    b9

---
+++ esercizio.b9/src/Main.java
@@ -0,0 +1,29 @@
+class Main {
+    public static void main(String[] args){
+
+        int v;
+        int first;           
+        boolean   notFound=true;
+
+        first=Keyboard.readInteger();
+
+        if (first!=0){
+
+            v=Keyboard.readInteger();
+            while (v!=0 && notFound){
+
+                if (v>first)
+                    notFound=false;
+
+                v=Keyboard.readInteger();
+            }
+        }
+
+        if (notFound)
+            Display.println("Non esiste nella lista un numero in valore maggiore del primo.");
+        else
+            Display.println("Esiste nella lista un numero in valore maggiore del primo.");
+    }
+
+
+}
