a87c65f4f5efb41c78be83fd81f199462f6cfd06
Leanid <leanid.herasimau@edu.unito.it>
2015-11-11 18:40:17 +0100

    b10

--- esercizio.b10/src/Main.java
+++ esercizio.b10/src/Main.java
@@ -1,32 +1,13 @@
 class Main {
     public static void main(String[] args){
 
-        // DATI DI INPUT:  una lista di valori interi positivi
-        // DATI DI OUTPUT: la media aritmetica dei valori
+        int        v;
+        final int primo;
+        boolean   tutti_non_minori=true;
 
-        // Definizione delle variabili
-
-        int        v;                    // memorizza l'ultimo valore letto
-        final int primo;                 // memorizza il primo numero della lista o il valore 0
-        // se la lista è vuota
-        boolean   tutti_non_minori=true; // variabile booelana che indica che
-        // "tutti i numeri in lista sono in valore non minori del primo"
-
-        // Testo del programma
-
-        Display.print("Inserisci una lista di valori interi positivi anche su più linee, termina la lista con il valore 0:\n> ");
-
-        // Quando il programma arriva ad eseguire l'istruzione che segue
-        // viene sospeso se l'utente non ha ancora inserito alcuna linea.
-        // Se l'utente è stato molto veloce e ha già battuto almeno una linea
-        // allora la prima chiamata readInteger() restituisce il primo
-        // valore intero presente nella linea.
-
-        primo=Keyboard.readInteger(); // leggo se esiste il primo numero della lista
-        // se la lista è vuota è letto il valore 0
-
+        primo=Keyboard.readInteger();
         if (primo!=0){
-            // la lista contiene almeno un valore
+
             v=Keyboard.readInteger();
             while (v!=0 && tutti_non_minori){
 
@@ -43,7 +24,4 @@ class Main {
             Display.println("Esiste nella lista un numero maggiore o uguale al primo.");
     }
 
-    /* Osservazioni:
-     *
-     */
 }
